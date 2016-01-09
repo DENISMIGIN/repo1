f2f41ef777aaf0ac8d5208e759a69d915d37d3b9
DENISMIGIN <>
2016-01-10 01:15:28 +0400

    ApexSQL Diff synchronization

--- Tables/HumanResources.Department.sql
+++ Tables/HumanResources.Department.sql
@@ -1,6 +1,9 @@
-SET ANSI_NULLS ON
+﻿SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
-SET ANSI_PADDING ON
+
+GO
+-- Create Table [HumanResources].[Department]
+Print 'Create Table [HumanResources].[Department]'
 GO
 CREATE TABLE [HumanResources].[Department] (
 		[DepartmentID]     [smallint] IDENTITY(1, 1) NOT NULL,
@@ -11,33 +14,64 @@ CREATE TABLE [HumanResources].[Department] (
 		PRIMARY KEY
 		CLUSTERED
 		([DepartmentID])
-	ON [PRIMARY]
-) ON [PRIMARY]
+)
+GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'CONSTRAINT', N'PK_Department_DepartmentID'
 GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'INDEX', N'PK_Department_DepartmentID'
+GO
+-- Add Default Constraint DF_Department_ModifiedDate to [HumanResources].[Department]
+Print 'Add Default Constraint DF_Department_ModifiedDate to [HumanResources].[Department]'
 GO
 ALTER TABLE [HumanResources].[Department]
 	ADD
 	CONSTRAINT [DF_Department_ModifiedDate]
 	DEFAULT (getdate()) FOR [ModifiedDate]
 GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'CONSTRAINT', N'DF_Department_ModifiedDate'
+GO
+-- Create Index AK_Department_Name on [HumanResources].[Department]
+Print 'Create Index AK_Department_Name on [HumanResources].[Department]'
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [AK_Department_Name]
 	ON [HumanResources].[Department] ([Name])
-	ON [PRIMARY]
+GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Unique nonclustered index.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'INDEX', N'AK_Department_Name'
 GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Primary key for Department records.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'COLUMN', N'DepartmentID'
+GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Name of the group to which the department belongs.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'COLUMN', N'GroupName'
 GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'COLUMN', N'ModifiedDate'
 GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
+GO
 EXEC sp_addextendedproperty N'MS_Description', N'Name of the department.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', 'COLUMN', N'Name'
+GO
+-- Create Extended Property MS_Description on [HumanResources].[Department]
+Print 'Create Extended Property MS_Description on [HumanResources].[Department]'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Lookup table containing the departments within the Adventure Works Cycles company.', 'SCHEMA', N'HumanResources', 'TABLE', N'Department', NULL, NULL
 GO
