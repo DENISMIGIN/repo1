283b7f2648644fea3d8d4f3b46abe8fecd7e6082
DENISMIGIN <>
2016-03-02 00:03:43 +0400

    ApexSQL Diff synchronization

---
+++ 5/Functions/dbo.fTestCase042.sql
@@ -0,0 +1,18 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create Function [dbo].[fTestCase042]
+Print 'Create Function [dbo].[fTestCase042]'
+GO
+/*@@objectName:[dbo]..[fTestCase042]*/
+
+create function fTestCase042
+(
+	@param1	as int
+)
+returns int
+as
+begin
+	return(dbo.fTestCase042a(@param1) * 2)
+end
+GO
---
+++ 5/Functions/dbo.fTestCase042a.sql
@@ -0,0 +1,18 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create Function [dbo].[fTestCase042a]
+Print 'Create Function [dbo].[fTestCase042a]'
+GO
+/*@@objectName:[dbo]..[fTestCase042a]*/
+
+CREATE function fTestCase042a
+(
+	@param1	as int
+)
+returns int
+as
+begin
+	return(@param1 * @param1)
+end
+GO
