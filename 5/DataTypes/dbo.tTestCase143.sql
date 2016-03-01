7d5c9927fc8490b9ef71437e9e3b3abd521b152c
DENISMIGIN <>
2016-03-02 01:38:13 +0400

    ApexSQL Diff synchronization

---
+++ 5/DataTypes/dbo.tTestCase143.sql
@@ -0,0 +1,13 @@
+﻿-- Create Type [dbo].[tTestCase143]
+Print 'Create Type [dbo].[tTestCase143]'
+GO
+CREATE TYPE [dbo].[tTestCase143]
+	FROM [varchar](24)
+	NOT NULL
+GO
+-- Add Permissions To The type [dbo].[tTestCase143]
+Print 'Add Permissions To The type [dbo].[tTestCase143]'
+GRANT REFERENCES
+	ON TYPE::[dbo].[tTestCase143]
+	TO [public]
+GO
