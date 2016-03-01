d88da0c189624713d78a114f8b7da5a1b73e6739
DENISMIGIN <>
2016-03-02 01:26:47 +0400

    ApexSQL Diff synchronization

---
+++ 5/Functions/dbo.fTestCase176.sql
@@ -0,0 +1,16 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create Function [dbo].[fTestCase176]
+Print 'Create Function [dbo].[fTestCase176]'
+GO
+/*@@objectName:[dbo]..[fTestCase176]*/
+
+create function fTestCase176
+()
+returns money
+as
+begin
+	return(10000)
+end
+GO
