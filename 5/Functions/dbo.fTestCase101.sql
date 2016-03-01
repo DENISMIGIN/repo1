a4d051b779d89f1c5b99dfc2cddbc15a059db4b8
DENISMIGIN <>
2016-03-01 23:09:52 +0400

    ApexSQL Diff synchronization

---
+++ 5/Functions/dbo.fTestCase101.sql
@@ -0,0 +1,19 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create Function [dbo].[fTestCase101]
+Print 'Create Function [dbo].[fTestCase101]'
+GO
+/*@@objectName:[dbo]..[fTestCase101]*/
+
+create function fTestCase101
+(
+	@param1		varchar(30),
+	@param2		varchar(30)
+)
+returns int
+as
+begin
+	return(columnproperty(object_id(@param1), @param2, 'precision'))
+end
+GO
