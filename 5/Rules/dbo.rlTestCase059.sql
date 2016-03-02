0ee3d5356b4821ee9965dd0571df45b31a9cb663
DENISMIGIN <>
2016-03-02 15:19:35 +0400

    ApexSQL Diff synchronization

---
+++ 5/Rules/dbo.rlTestCase059.sql
@@ -0,0 +1,9 @@
+﻿-- Create Rule [dbo].[rlTestCase059]
+Print 'Create Rule [dbo].[rlTestCase059]'
+GO
+/*@@objectName:[dbo]..[rlTestCase059]*/
+
+create rule rlTestCase059
+as
+upper(@value) in ('A', 'B', 'C', 'D', 'E')
+GO
