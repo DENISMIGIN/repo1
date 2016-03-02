784c1141b1adc510da53c07fe9b09385a975e240
DENISMIGIN <>
2016-03-02 15:17:13 +0400

    ApexSQL Diff synchronization

---
+++ 5/Rules/dbo.rlTestCase058.sql
@@ -0,0 +1,9 @@
+﻿-- Create Rule [dbo].[rlTestCase058]
+Print 'Create Rule [dbo].[rlTestCase058]'
+GO
+/*@@objectName:[dbo]..[rlTestCase058]*/
+
+create rule rlTestCase058
+as
+@value > 1000 and @value < 2000
+GO
