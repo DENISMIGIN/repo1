6ad5fbb1cace432627d3ecd92c328b09b1b43a89
DENISMIGIN <>
2016-03-13 13:43:27 +0400

    ApexSQL Diff synchronization

---
+++ 11/Schemas/Sales.sql
@@ -0,0 +1,11 @@
+﻿-- Create Schema [Sales]
+Print 'Create Schema [Sales]'
+GO
+CREATE SCHEMA [Sales]
+	AUTHORIZATION [dbo]
+GO
+-- Create Extended Property MS_Description on [Sales]
+Print 'Create Extended Property MS_Description on [Sales]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Contains objects related to customers, sales orders, and sales territories.', 'SCHEMA', N'Sales', NULL, NULL, NULL, NULL
+GO
