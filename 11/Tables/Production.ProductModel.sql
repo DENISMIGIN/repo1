cb88f9393538ef52eb2ecfde191e5da3d51984c8
DENISMIGIN <>
2016-03-13 23:13:39 +0400

    ApexSQL Diff synchronization

---
+++ 11/Tables/Production.ProductModel.sql
@@ -0,0 +1,137 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+-- Create Table [Production].[ProductModel]
+Print 'Create Table [Production].[ProductModel]'
+GO
+CREATE TABLE [Production].[ProductModel] (
+		[ProductModelID]         [int] IDENTITY(1, 1) NOT NULL,
+		[Name]                   [dbo].[Name] NOT NULL,
+		[CatalogDescription]     [xml](CONTENT [Production].[ProductDescriptionSchemaCollection]) NULL,
+		[Instructions]           [xml](CONTENT [Production].[ManuInstructionsSchemaCollection]) NULL,
+		[rowguid]                [uniqueidentifier] NOT NULL ROWGUIDCOL,
+		[ModifiedDate]           [datetime] NOT NULL,
+		CONSTRAINT [PK_ProductModel_ProductModelID]
+		PRIMARY KEY
+		CLUSTERED
+		([ProductModelID])
+)
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'CONSTRAINT', N'PK_ProductModel_ProductModelID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'INDEX', N'PK_ProductModel_ProductModelID'
+GO
+-- Add Default Constraint DF_ProductModel_ModifiedDate to [Production].[ProductModel]
+Print 'Add Default Constraint DF_ProductModel_ModifiedDate to [Production].[ProductModel]'
+GO
+ALTER TABLE [Production].[ProductModel]
+	ADD
+	CONSTRAINT [DF_ProductModel_ModifiedDate]
+	DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'CONSTRAINT', N'DF_ProductModel_ModifiedDate'
+GO
+-- Add Default Constraint DF_ProductModel_rowguid to [Production].[ProductModel]
+Print 'Add Default Constraint DF_ProductModel_rowguid to [Production].[ProductModel]'
+GO
+ALTER TABLE [Production].[ProductModel]
+	ADD
+	CONSTRAINT [DF_ProductModel_rowguid]
+	DEFAULT (newid()) FOR [rowguid]
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of NEWID()', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'CONSTRAINT', N'DF_ProductModel_rowguid'
+GO
+-- Create Index AK_ProductModel_Name on [Production].[ProductModel]
+Print 'Create Index AK_ProductModel_Name on [Production].[ProductModel]'
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_ProductModel_Name]
+	ON [Production].[ProductModel] ([Name])
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Unique nonclustered index.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'INDEX', N'AK_ProductModel_Name'
+GO
+-- Create Index AK_ProductModel_rowguid on [Production].[ProductModel]
+Print 'Create Index AK_ProductModel_rowguid on [Production].[ProductModel]'
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_ProductModel_rowguid]
+	ON [Production].[ProductModel] ([rowguid])
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Unique nonclustered index. Used to support replication samples.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'INDEX', N'AK_ProductModel_rowguid'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Detailed product catalog information in xml format.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'CatalogDescription'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Manufacturing instructions in xml format.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'Instructions'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'ModifiedDate'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Product model description.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'Name'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key for ProductModel records.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'ProductModelID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'COLUMN', N'rowguid'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Product model classification.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', NULL, NULL
+GO
+-- Create Xml Index PXML_ProductModel_CatalogDescription on [Production].[ProductModel]
+Print 'Create Xml Index PXML_ProductModel_CatalogDescription on [Production].[ProductModel]'
+GO
+CREATE PRIMARY XML INDEX [PXML_ProductModel_CatalogDescription]
+	ON [Production].[ProductModel] ([CatalogDescription])
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary XML index.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'INDEX', N'PXML_ProductModel_CatalogDescription'
+GO
+-- Create Xml Index PXML_ProductModel_Instructions on [Production].[ProductModel]
+Print 'Create Xml Index PXML_ProductModel_Instructions on [Production].[ProductModel]'
+GO
+CREATE PRIMARY XML INDEX [PXML_ProductModel_Instructions]
+	ON [Production].[ProductModel] ([Instructions])
+GO
+-- Create Extended Property MS_Description on [Production].[ProductModel]
+Print 'Create Extended Property MS_Description on [Production].[ProductModel]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary XML index.', 'SCHEMA', N'Production', 'TABLE', N'ProductModel', 'INDEX', N'PXML_ProductModel_Instructions'
+GO
+ALTER TABLE [Production].[ProductModel] SET (LOCK_ESCALATION = TABLE)
+GO
---
+++ 11/Tables/Production.ProductProductPhoto.sql
@@ -0,0 +1,113 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+-- Create Table [Production].[ProductProductPhoto]
+Print 'Create Table [Production].[ProductProductPhoto]'
+GO
+CREATE TABLE [Production].[ProductProductPhoto] (
+		[ProductID]          [int] NOT NULL,
+		[ProductPhotoID]     [int] NOT NULL,
+		[Primary]            [dbo].[Flag] NOT NULL,
+		[ModifiedDate]       [datetime] NOT NULL,
+		CONSTRAINT [PK_ProductProductPhoto_ProductID_ProductPhotoID]
+		PRIMARY KEY
+		NONCLUSTERED
+		([ProductID], [ProductPhotoID])
+)
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'CONSTRAINT', N'PK_ProductProductPhoto_ProductID_ProductPhotoID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Nonclustered index created by a primary key constraint.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'INDEX', N'PK_ProductProductPhoto_ProductID_ProductPhotoID'
+GO
+-- Add Default Constraint DF_ProductProductPhoto_ModifiedDate to [Production].[ProductProductPhoto]
+Print 'Add Default Constraint DF_ProductProductPhoto_ModifiedDate to [Production].[ProductProductPhoto]'
+GO
+ALTER TABLE [Production].[ProductProductPhoto]
+	ADD
+	CONSTRAINT [DF_ProductProductPhoto_ModifiedDate]
+	DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'CONSTRAINT', N'DF_ProductProductPhoto_ModifiedDate'
+GO
+-- Add Default Constraint DF_ProductProductPhoto_Primary to [Production].[ProductProductPhoto]
+Print 'Add Default Constraint DF_ProductProductPhoto_Primary to [Production].[ProductProductPhoto]'
+GO
+ALTER TABLE [Production].[ProductProductPhoto]
+	ADD
+	CONSTRAINT [DF_ProductProductPhoto_Primary]
+	DEFAULT ((0)) FOR [Primary]
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of 0 (FALSE)', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'CONSTRAINT', N'DF_ProductProductPhoto_Primary'
+GO
+-- Create Foreign Key FK_ProductProductPhoto_Product_ProductID on [Production].[ProductProductPhoto]
+Print 'Create Foreign Key FK_ProductProductPhoto_Product_ProductID on [Production].[ProductProductPhoto]'
+GO
+ALTER TABLE [Production].[ProductProductPhoto]
+	WITH CHECK
+	ADD CONSTRAINT [FK_ProductProductPhoto_Product_ProductID]
+	FOREIGN KEY ([ProductID]) REFERENCES [Production].[Product] ([ProductID])
+ALTER TABLE [Production].[ProductProductPhoto]
+	CHECK CONSTRAINT [FK_ProductProductPhoto_Product_ProductID]
+
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Foreign key constraint referencing Product.ProductID.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'CONSTRAINT', N'FK_ProductProductPhoto_Product_ProductID'
+GO
+-- Create Foreign Key FK_ProductProductPhoto_ProductPhoto_ProductPhotoID on [Production].[ProductProductPhoto]
+Print 'Create Foreign Key FK_ProductProductPhoto_ProductPhoto_ProductPhotoID on [Production].[ProductProductPhoto]'
+GO
+ALTER TABLE [Production].[ProductProductPhoto]
+	WITH CHECK
+	ADD CONSTRAINT [FK_ProductProductPhoto_ProductPhoto_ProductPhotoID]
+	FOREIGN KEY ([ProductPhotoID]) REFERENCES [Production].[ProductPhoto] ([ProductPhotoID])
+ALTER TABLE [Production].[ProductProductPhoto]
+	CHECK CONSTRAINT [FK_ProductProductPhoto_ProductPhoto_ProductPhotoID]
+
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Foreign key constraint referencing ProductPhoto.ProductPhotoID.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'CONSTRAINT', N'FK_ProductProductPhoto_ProductPhoto_ProductPhotoID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'COLUMN', N'ModifiedDate'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'0 = Photo is not the principal image. 1 = Photo is the principal image.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'COLUMN', N'Primary'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Product identification number. Foreign key to Product.ProductID.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'COLUMN', N'ProductID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Product photo identification number. Foreign key to ProductPhoto.ProductPhotoID.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', 'COLUMN', N'ProductPhotoID'
+GO
+-- Create Extended Property MS_Description on [Production].[ProductProductPhoto]
+Print 'Create Extended Property MS_Description on [Production].[ProductProductPhoto]'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Cross-reference table mapping products and product photos.', 'SCHEMA', N'Production', 'TABLE', N'ProductProductPhoto', NULL, NULL
+GO
+ALTER TABLE [Production].[ProductProductPhoto] SET (LOCK_ESCALATION = TABLE)
+GO
