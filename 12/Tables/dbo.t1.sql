f7685b6fd531cf14a36f4de0f9082a023d1fc2d1
DENISMIGIN <>
2016-04-15 23:38:44 +0400

    ApexSQL Diff synchronization

---
+++ 12/Tables/dbo.t1.sql
@@ -0,0 +1,16 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+-- Create Table [dbo].[t1]
+Print 'Create Table [dbo].[t1]'
+GO
+CREATE TABLE [dbo].[t1] (
+		[c1]     [int] NULL,
+		[c2]     [int] NULL,
+		[c3]     [int] NULL,
+		[c4]     [int] NULL
+)
+GO
+ALTER TABLE [dbo].[t1] SET (LOCK_ESCALATION = TABLE)
+GO
