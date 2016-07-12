41ef46eee994dded88c280c5dbcff0b171e8f1d7
DENISMIGIN <>
2016-07-12 23:46:36 +0300

    ApexSQL Diff synchronization123

---
+++ db1/Functions/dbo.fTestCase001.sql
@@ -0,0 +1,17 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create function [dbo].[fTestCase001]
+Print 'Create function [dbo].[fTestCase001]'
+GO
+create function fTestCase001
+(
+	@param1		nvarchar(10),
+	@param2		nvarchar(20)
+)
+returns nvarchar(60)
+as
+begin
+	return(@param1 + @param2)
+end
+GO
---
+++ db1/Tables/dbo.TestCase001.sql
@@ -0,0 +1,15 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+-- Create table [dbo].[TestCase001]
+Print 'Create table [dbo].[TestCase001]'
+GO
+CREATE TABLE [dbo].[TestCase001] (
+		[f1]     [nvarchar](10) COLLATE Cyrillic_General_CI_AS NULL,
+		[f2]     [nvarchar](20) COLLATE Cyrillic_General_CI_AS NULL,
+		[f3]     AS ([dbo].[fTestCase001]([f1],[f2]))
+)
+GO
+ALTER TABLE [dbo].[TestCase001] SET (LOCK_ESCALATION = TABLE)
+GO
